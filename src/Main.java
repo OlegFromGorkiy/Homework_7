ecce48e75ea969545d9bf1632bfc8019587de88a
Oleg Kartashev <officinator@gmail.com>
2022-09-08 20:51:28 +0300

    All tasks are complete!

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-//        task_01();
-//        task_02();
-//        task_03();
-//        task_04();
-//        task_05();
+        long startTime = System.nanoTime();
+        task_01();
+        task_02();
+        task_03();
+        task_04();
+        task_05();
         task_06();
+        task_07();
+        task_08();
+        task_09();
+        task_11();
+        task_12();
+        long elapsedTime = System.nanoTime() - startTime;
+        System.out.println("Эатрачено " + elapsedTime + " наносекунд");
     }
 
     public static void task_01() {
@@ -78,21 +86,85 @@ public class Main {
                     count, bankAccount);
 
         }
-        System.out.printf("Месяц %d, накопления Василия составляют %d.\n",count, bankAccount);
+        System.out.printf("Месяц %d, накопления Василия составляют %d.\n", count, bankAccount);
         System.out.println("***\n");
     }
+
     public static void task_06() {
         System.out.println("Задание 6");
         int bankAccount = 15_000;
         int percent = 7;
-        for (int i = 1; i <=9 ; i++) {
-            for (int j = 1; j <=12 ; j++) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 12; j++) {
                 bankAccount = bankAccount + bankAccount * percent / 100;
                 if (j % 6 == 0) System.out.printf("Год %d, месяц %d, накопления Василия составляют %d.\n",
-                        i,j, bankAccount);
+                        i, j, bankAccount);
             }
         }
+        System.out.println("***\n");
+    }
 
+    public static void task_07() {
+        System.out.println("Задание 7");
+        int firstFriday = (int) Math.round(1 + Math.random() * 6);
+        for (int i = firstFriday; i <= 31; i += 7) {
+            System.out.printf("Сегодня пятница, %d-е число. Необходимо подготовить отчет.\n", i);
+        }
+        System.out.println("***\n");
+    }
+
+    public static void task_08() {
+        System.out.println("Задание 8");
+        int period = 79;
+        int thisYear = 2022;
+        int nextAppearance = 0;
+        for (int i = thisYear - 200; i < thisYear; i++) {
+            if (i % period == 0) {
+                for (int j = i; j <= thisYear; j += period) {
+                    System.out.println(j);
+                    nextAppearance = j + period;
+                }
+                break;
+            }
+        }
+        System.out.println(nextAppearance);
+        System.out.println("***\n");
+    }
+
+    public static void task_09() {
+        System.out.println("Задание 9");
+        for (int i = 1; i < 11; i++) {
+            System.out.printf("2x%d=%d\n", i, i * 2);
+        }
+        System.out.println("***\n");
+    }
+
+    public static void task_11() {
+        System.out.println("\"Сложное\" задание 1");
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i < 31; i++) {
+            builder.append(i).append(':');
+            if (i % 3 == 0) builder.append(" ping");
+            if (i % 5 == 0) builder.append(" pong");
+            System.out.println(builder);
+            builder = new StringBuilder();//.delete(0,builder.length());
+        }
+        System.out.println("***\n");
+    }
+
+    public static void task_12() {
+        System.out.println("\"Сложное\" задание 2");
+        int first = 0;
+        int second = 1;
+        StringBuilder builder = new StringBuilder();
+        builder.append(first).append(" ").append(second);
+        for (int i = 2; i < 10; i++) {
+            int next = first + second;
+            builder.append(" ").append(next);
+            first = second;
+            second = next;
+        }
+        System.out.println(builder);
         System.out.println("***\n");
     }
 }
